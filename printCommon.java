910051adca08dab480e7dcabd43bb708b5164a52
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-06-14 12:42:00 +0530

    common elements between two array

--- printCommon.java
+++ printCommon.java
@@ -1,5 +1,18 @@
 public class printCommon {
     public static void main(String[] args) {
-        
+        int[] arr1={5,7,3,2,1};
+        int[] arr2={9,4,3,5,0,8,6,5};
+
+
+        for(int i =0;i<arr1.length;i++){
+
+            for(int j=0;j<arr2.length;j++){
+
+                if(arr1[i]==arr2[j]){
+                    System.out.println(arr1[i]);
+                    break;
+                }
+            }
+        }
     }
 }
